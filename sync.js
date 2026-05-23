aab85e5de6987c657b2a5c2cc2d70ea99064f1e3
FindAndDriveSupport <support@findndrive.co.za>
2026-05-23 08:09:18 +0200

    Update sync.js

--- sync.js
+++ sync.js
@@ -234,9 +234,6 @@ async function createHubSpotContact(lead, intent, kredoResult = null) {
     seriti_dealer_name:     lead.dealerName,
     seriti_dealer_code:     lead.dealerCode,
     seriti_lead_date:       lead.date,
-    seriti_first_name:      lead.firstName,
-    seriti_last_name:       lead.lastName,
-    seriti_mobile_number:   lead.mobileNumber,
     seriti_id_number:       lead.idNumber,
     estimated_finance:      lead.estimatedAmount,
   };
